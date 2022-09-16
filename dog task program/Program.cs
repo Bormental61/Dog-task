d4ff5a437c5b379ef58a320a19c7646bdb50a1a1
Bormental <tyranosaurus@yandex.ru>
2022-09-16 12:39:39 +0300

    Program for Dog task done

---
+++ dog task program/Program.cs
@@ -0,0 +1,33 @@
+﻿// dog task from Введение в программирование семинар 1
+
+System.Console.WriteLine("Два друга идут навстречу друг другу. Между ними бегает собака. Каждый раз, когда собака добегает до одного друга, она разворачивается и бежит к другому. Нужно вычислить сколько раз собака пробежит между друзьями, пока они не встретятся.");
+
+Console.Write("Введите расстояние между друзьями: ");
+double distance = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость первого друга: ");
+double firstFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость второго друга: ");
+double secondFriendSpeed = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите скорость собаки: ");
+double dogSpeed = Convert.ToDouble(Console.ReadLine());
+double count = 0;
+double friend = 2;
+double time = 0;
+
+while(distance > 3)
+{
+    if(friend == 1)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed)*time;
+    count++;
+}
+
+System.Console.WriteLine($"Ответ: Собака пробежит между друзьями {count} раз.");
